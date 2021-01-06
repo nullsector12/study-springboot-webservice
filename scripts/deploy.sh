2834a4cd403a8d041ec3aa914ae83b824cc90ec3
nullsector12 <rlamd0412@gmail.com>
2021-01-06 13:48:21 +0900

    CodeDeploy 자동 배포 연동을 위헤 deploy.sh 오류 수정 (step2)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,11 +4,12 @@ REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=study-springboot-webservice
 
 echo "> Build 파일 복사"
+
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 PID 확인"
 
-CURRENT_PID=$(pgrep -f1 study-springboot-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl study-springboot-webservice | grep jar | awk '{print $1}')
 
 echo "> 현재 구동중인 애플리케이션 PID : $CURRENT_PID"
 
@@ -16,6 +17,7 @@ if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
+  # shellcheck disable=SC2086
   kill -15 $CURRENT_PID
   sleep 5
 fi
